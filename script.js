041094d57a421c0dc84a461866ce1d5270da6bf4
JahanaviChaturvedi <118982130+JahanaviChaturvedi@users.noreply.github.com>
2025-03-06 10:28:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+document.getElementById("btn").onClick = function(){
+	const inputValue = Number(document.getElementById("ip").value);
+	const outputValue = document.getElementById("output"});
+} 
