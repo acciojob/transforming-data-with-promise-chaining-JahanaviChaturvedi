f2ec197811002244656d108b43d7b064aebc8268
JahanaviChaturvedi <118982130+JahanaviChaturvedi@users.noreply.github.com>
2025-03-06 10:31:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,4 +2,11 @@
 document.getElementById("btn").onClick = function(){
 	const inputValue = Number(document.getElementById("ip").value);
 	const outputValue = document.getElementById("output"});
-} 
+
+	new Promise((resolve, reject)=>{
+		setTimeout(()=>{
+			outputDiv.innerHTML = `Result: ${inputValue}`;
+			resolve(inputValue);
+		},2000);
+	});
+}
